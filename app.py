dbefa3bb194c6e5644cd0b2fcb11f2bf59ef1f94
btambalo-dotcom <btambalo@gmail.com>
2025-09-13 19:44:37 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ app.config["MAX_CONTENT_LENGTH"] = max_len_mb * 1024 * 1024
 DB_PATH = os.path.join(BASE_DIR, "app.db")
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, timeout=30)
     conn.row_factory = sqlite3.Row
     return conn
 
