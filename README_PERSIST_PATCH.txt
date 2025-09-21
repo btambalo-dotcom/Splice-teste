e5f8f99488137b993f11b6ebfe4e9e0098495cdd
btambalo-dotcom <btambalo@gmail.com>
2025-09-21 19:30:32 -0400

    a

--- README_PERSIST_PATCH.txt
+++
@@ -1,1 +0,0 @@
-Patch de persistência aplicado. Use DATA_DIR=/var/data e (opcional) DATABASE_URL=sqlite:////var/data/splice.db.
--- sitecustomize.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Executa no import automático do Python quando presente no sys.path.
-from persist_helper import ensure_persist  # noqa: F401
-ensure_persist()
